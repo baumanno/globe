0684e905f49017417592401390de75e34f6a510e
William Papper <contact@papper.me>
2014-08-01 23:52:18 -0400

    Revert "Added build and test files to eliminate workarounds"
    Adding build and test files did not help the issue when working with a fresh repo, even though it appeared to during testing.
    This reverts commit 29c7af74ba2b90e9c9fddbc3ea34f45b221db3c3.

--- build/tmp/js/components/LoadingIndicatorComponent.js
+++
@@ -1,9 +0,0 @@
-/*global GLOBE, Em */
-GLOBE.LoadingIndicatorComponent = Em.Component.extend({
-    isDataLoaded: function(){
-
-        // change isDataLoaded depending on GLOBE.loading number
-        return (GLOBE.get('loading') <= 0);
-
-    }.property('GLOBE.loading')
-});
--- build/tmp/js/mixins/ContentReadyTriggerableMixin.js
+++
@@ -1,9 +0,0 @@
-/*global $, GLOBE, Em */
-GLOBE.ContentReadyTriggerableMixin = Em.Mixin.create({
-    /**
-     * Function that is called if the controller content is changed.
-     */
-    contentChanged: function(){
-        this.trigger('content-ready');
-    }.observes('content')
-});
